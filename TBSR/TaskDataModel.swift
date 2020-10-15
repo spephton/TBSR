03f6e0953eb1978b4afe37710ac46b9dbdf9f986
Jacob Sephton <jacob.sephton@gmail.com>
2020-10-15 21:43:39 +1100

    Add TaskDataModel.swift class definition

---
+++ TBSR/TaskDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  TaskDataModel.swift
+//  TBSR
+//
+//  Created by Jacob Sephton on 15/10/20.
+//
+
+import Foundation
+
+class TaskDataModel {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
